8732d272ca2f333a8b90c9d9243cf7806ec57028
ZhengRyan <ryan.zhengrp@gmail.com>
2023-07-17 20:56:59 +0800

    update version

--- treemodel2sql/__init__.py
+++ treemodel2sql/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """RyanZheng"""
 __email__ = 'zhengruiping000@163.com'
-__version__ = '0.1.0'
+__version__ = '0.1.2'
 
 from .xgboost2sql import XGBoost2Sql
 from .lgb2sql import Lgb2Sql
