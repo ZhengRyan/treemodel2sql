7860df52281346398cc39987c66600d720684b78
ZhengRyan <ryan.zhengrp@gmail.com>
2023-08-09 22:01:24 +0800

    update code, add round_decimal argument

--- treemodel2sql/__init__.py
+++ treemodel2sql/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """RyanZheng"""
 __email__ = 'zhengruiping000@163.com'
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 from .xgboost2sql import XGBoost2Sql
 from .lgb2sql import Lgb2Sql
